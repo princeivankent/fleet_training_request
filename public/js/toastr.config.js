c694bdf0640b72b2b2517e329d9cabb051b6f446
Prince Ivan Kent Tiburcio <princeivankentmtiburcio@gmail.com>
2018-10-05 10:06:39 +0800

    Application Dependencies Added

---
+++ public/js/toastr.config.js
@@ -0,0 +1,9 @@
+toastr.options = {
+    'positionClass': 'toast-bottom-right',
+    'showMethod': 'slideDown',
+    'hideMethod': 'slideUp',
+    'closeMethod': 'slideUp',
+    'progressBar': true,
+    'timeOut': 8000,
+    'extendedTimeOut': 4000
+}
--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -5,8 +5,5 @@ window.Vuex = require('vuex');
 window.VueScrollTo = require('vue-scrollto');
 window.Swal = require('sweetalert');
 
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
 Vue.use(Vuex);
 Vue.use(VueScrollTo);
