c33071a0d0927a2e03cd8f16b7012510bdb3aa86
Prince Ivan Kent Tiburcio <princeivankentmtiburcio@gmail.com>
2018-10-10 08:44:06 +0800

    Approver Status created

---
+++ app/Approver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Approver extends Model
+{
+	protected $fillable = ['approver_name','status'];
+	protected $primaryKey = 'approver_id';
+	public $timestamps = false;
+
+	public function approval_statuses()
+	{
+		return $this->hasMany('App\ApprovalStatus', 'approve_id', 'approver_id');
+	}
+}
