fc83a6e1e274b38a90282e8c365b498c9767b6fb
Prince Ivan Kent Tiburcio <princeivankentmtiburcio@gmail.com>
2018-11-12 14:11:20 +0800

    Admin disapprove action bugfixed

--- app/Http/Controllers/ApproveRequestController.php
+++ app/Http/Controllers/ApproveRequestController.php
@@ -28,34 +28,39 @@ class ApproveRequestController extends Controller
 
             // If $query == 1 email sent
             if ($query) {
-                $approvers = Approver::all();
-    
-                foreach ($approvers as $value) {
-                    $approval_status = new ApprovalStatus;
-                    $approval_status->training_request_id = $training_request->training_request_id;
-                    $approval_status->approver_id = $value['approver_id'];
-                    $approval_status->save();
-
-                    $approver_id = $approval_status->approver_id;
-                    $approver = Approver::findOrFail($approver_id);
-                    $training_program = TrainingProgram::findOrFail($training_request->training_program_id);
-
-                    $batch_mails->save_to_batch([
-                        'email_category_id' => config('constants.superior_approval'),
-                        'subject' => 'Training Request Approval',
-                        'sender' => config('mail.from.address'),
-                        'recipient' => $approver->email,
-                        'title' => 'Training Request Approval',
-                        'message' => 'Greetings! '. $training_request->contact_person .' of <strong>'. $training_request->company_name .'</strong> is requesting for a <br/>
-						training program: '. $training_program->program_title .' <br/>
-						on '. Carbon::parse($training_request->training_date)->format('M d, Y D - h:i A'),
-						'cc' => null,
-                        'attachment' => null,
-                        'accept_url' => route('superior_approval', ['approval_status_id' => $approval_status->approval_status_id]),
-                        'deny_url' => route('superior_disapproval', ['approval_status_id' => $approval_status->approval_status_id])
-                    ]);
+                if ($request->request_status == 'denied') {
+                    return response()->json($query);
                 }
-                return response()->json($query);
+                else {
+                    $approvers = Approver::all();
+    
+                    foreach ($approvers as $value) {
+                        $approval_status = new ApprovalStatus;
+                        $approval_status->training_request_id = $training_request->training_request_id;
+                        $approval_status->approver_id = $value['approver_id'];
+                        $approval_status->save();
+
+                        $approver_id = $approval_status->approver_id;
+                        $approver = Approver::findOrFail($approver_id);
+                        $training_program = TrainingProgram::findOrFail($training_request->training_program_id);
+
+                        $batch_mails->save_to_batch([
+                            'email_category_id' => config('constants.superior_approval'),
+                            'subject' => 'Training Request Approval',
+                            'sender' => config('mail.from.address'),
+                            'recipient' => $approver->email,
+                            'title' => 'Training Request Approval',
+                            'message' => 'Greetings! '. $training_request->contact_person .' of <strong>'. $training_request->company_name .'</strong> is requesting for a <br/>
+                            training program: '. $training_program->program_title .' <br/>
+                            on '. Carbon::parse($training_request->training_date)->format('M d, Y D - h:i A'),
+                            'cc' => null,
+                            'attachment' => null,
+                            'accept_url' => route('superior_approval', ['approval_status_id' => $approval_status->approval_status_id]),
+                            'deny_url' => route('superior_disapproval', ['approval_status_id' => $approval_status->approval_status_id])
+                        ]);
+                    }
+                    return response()->json($query);
+                }
             }
         }
 
@@ -64,7 +69,7 @@ class ApproveRequestController extends Controller
     public function update_approval_request($approval_status_id, $status)
     {
         $query = ApprovalStatus::findOrFail($approval_status_id);
-        $query->status = 'approved';
+        $query->status = $status;
         $query->save();
 
         return response()->json($query);
