40251e48a4a9cdec8fad52257f92a7041c56cff5
Prince Ivan Kent Tiburcio <princeivankentmtiburcio@gmail.com>
2018-11-12 17:14:51 +0800

    TrainorDesignation added model

---
+++ app/TrainorDesignation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TrainorDesignation extends Model
+{
+    protected $fillable = ['training_request_id','trainor_id','assigned_by'];
+    protected $primaryKey = 'trainor_designation_id';
+    public $timestamps = false;
+
+    public function training_request()
+    {
+        return $this->belongsTo('App\TrainingRequest', 'training_request_id', 'training_request_id');
+    }
+
+    public function trainor()
+    {
+        return $this->belongsTo('App\Trainor', 'trainor_id', 'trainor_id');
+    }
+}
