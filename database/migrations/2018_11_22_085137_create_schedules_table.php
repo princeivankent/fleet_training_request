4a09b2b4304bd6ae2c30b5bfb5628a432db1784a
(MIS)-Prince Ivan Kent M. Tiburcio <prince-tiburcio@isuzuphil.com>
2018-11-22 09:19:22 +0800

    Environment Updated

---
+++ database/migrations/2018_11_22_085137_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+
+            $table->increments('schedule_id');
+            $table->date('date');
+            $table->text('reason');
+            $table->string('created_by', 100);
+            $table->unsignedInteger('training_schedule_id');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
