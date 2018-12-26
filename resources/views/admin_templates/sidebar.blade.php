f8fe089156ee16d67453efafba7eb30b60ba97be
princeivankent <princeivankentmtiburcio@gmail.com>
2018-12-26 17:43:51 +0800

    Calendar menu adjusted

--- resources/views/admin_templates/sidebar.blade.php
+++ resources/views/admin_templates/sidebar.blade.php
@@ -57,15 +57,5 @@
             <i class="fa fa-caret-right"></i>&nbsp; 
             <span>Unit Models</span>
         </a></li>
-
-        <li class="header">SCHEDULES</li>
-        <li id="schedule_tab"><a href="{{ route('schedules') }}">
-            <i class="fa fa-caret-right"></i>&nbsp; 
-            <span>Marked Dates</span>
-        </a></li>
-        <li id="calendar_tab"><a href="{{ route('calendar') }}">
-            <i class="fa fa-calendar"></i>&nbsp; 
-            <span>Calendar</span>
-        </a></li>
     </ul>
 </section>
