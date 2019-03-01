719d005dac55e1a86427476df1ce4076fb6666e3
princeivankent <princeivankentmtiburcio@gmail.com>
2019-03-01 15:57:58 +0800

    Configured for hot module replacement

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,11 +4,22 @@ mix.setResourceRoot('/fleet_training_request/public/')
 .js('resources/assets/js/admin.js', 'public/js/admin.js')
 .js('resources/assets/js/client.js', 'public/js/client.js')
 .js('resources/src/main.js', 'public/js/app.js')
-.extract(['vue'])
 .browserSync('http://localhost/fleet_training_request/')
 .disableNotifications();
 
-/**
- * mix.copy('node_modules/material-design-icons-iconfont/dist/material-design-icons.css', 'public/css/material-design-icons.css').sourceMaps();
- * mix.copy('node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'public/css/font-awesome.css').sourceMaps();
- */
+Mix.listen('configReady', (webpackConfig) => {
+  if (Mix.isUsing('hmr')) {
+    // Remove leading '/' from entry keys
+    webpackConfig.entry = Object.keys(webpackConfig.entry).reduce((entries, entry) => {
+      entries[entry.replace(/^\//, '')] = webpackConfig.entry[entry];
+      return entries;
+    }, {});
+
+    // Remove leading '/' from ExtractTextPlugin instances
+    webpackConfig.plugins.forEach((plugin) => {
+      if (plugin.constructor.name === 'ExtractTextPlugin') {
+        plugin.filename = plugin.filename.replace(/^\//, '');
+      }
+    });
+  }
+});
